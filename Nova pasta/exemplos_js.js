71f2f4f3414eb1225622648b37c16a733aad9113
vivianhds <vvnhenriquee@gmail.com>
2018-08-30 22:45:17 -0300

    Javascript3

---
+++ Nova pasta/exemplos_js.js
@@ -0,0 +1,11 @@
+//Escrever os números de 0 a 10 com laço de repetição
+var i;
+for(i = 0; i <=10; i++) {
+	document.write(i + " ");
+}
+ 
+//Escrever a tabuada do 2 
+var i;
+for(i = 0; i <=10; i**) {
+	document.write(2*i +"<br>" );
+}
---
+++ Nova pasta/js/exemplos_js.js
@@ -0,0 +1,7 @@
+//Escrever os números de 0 a 10 com laço de repetição
+var i;
+for(i = 0; i <=10; i++) {
+	document.write(i + " ");
+}
+ 
+//Escrever a tabuada do 2 
